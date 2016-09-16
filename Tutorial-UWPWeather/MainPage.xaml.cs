2d8b6ba4e3e8cf41fca885e9e37c4ea53656219c
Rogger Matamoros <roggmatz@gmail.com>
2016-09-16 11:03:50 -0400

    Added error handling when fetching location and weather. Location and weather info now fetched on app load instead of on button click.

--- Tutorial-UWPWeather/MainPage.xaml.cs
+++ Tutorial-UWPWeather/MainPage.xaml.cs
@@ -28,32 +28,39 @@ namespace Tutorial_UWPWeather
             this.InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e) {
-            var position = await LocationManager.GetPosition();
+        private async void Page_Loaded(object sender, RoutedEventArgs e) {
+             Windows.Devices.Geolocation.Geoposition position;
+            try {
+                position = await LocationManager.GetPosition();
+                //get the weather from OpenWeatherMap API. For now we are just testing with a hard-coded string
+                //so the coordinates passed are not used and therefore meaningless.
+                RootObject weather = await OpenWeatherMapProxy.GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
 
-            //get the weather from OpenWeatherMap API. For now we are just testing with a hard-coded string
-            //so the coordinates passed are not used and therefore meaningless.
-            RootObject weather = await OpenWeatherMapProxy.GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
+                //schedule updates from LiveTileService
+                string URI = String.Format(
+                    "http://livetileservice1462.azurewebsites.net/default/?lat={0}&lon={1}",
+                    position.Coordinate.Latitude,
+                    position.Coordinate.Longitude
+                    );
 
-            //schedule updates from LiveTileService
-            string URI = String.Format(
-                "http://livetileservice1462.azurewebsites.net/default/?lat={0}&lon={1}",
-                position.Coordinate.Latitude,
-                position.Coordinate.Longitude
-                );
+                var tileContent = new Uri(URI);
+                var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+                updater.StartPeriodicUpdate(tileContent, PeriodicUpdateRecurrence.HalfHour);
 
-            var tileContent = new Uri(URI);
-            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
-            updater.StartPeriodicUpdate(tileContent, PeriodicUpdateRecurrence.HalfHour);
-            
-            //get the actual icon image referenced in the prior API call
-            string icon = String.Format("http://openweathermap.org/img/w/{0}.png", weather.weather[0].icon);
+                //get the actual icon image referenced in the prior API call
+                string icon = String.Format("http://openweathermap.org/img/w/{0}.png", weather.weather[0].icon);
 
-            //Assign the icon fetched to the source of the XAML image element in MainPage
-            ResultImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(icon, UriKind.Absolute));
+                //Assign the icon fetched to the source of the XAML image element in MainPage
+                ResultImage.Source = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new Uri(icon, UriKind.Absolute));
 
-            //Display today's forecast in the XAML text element.
-            ResultTextBlock.Text = weather.name + " - " + weather.main.temp + " - " + weather.weather[0].description;
+                //Display today's forecast in the XAML text element.
+                TemperatureTextBlock.Text = weather.main.temp.ToString();
+                DescriptionTextBlock.Text = weather.weather[0].description;
+                LocationTextBlock.Text = weather.name;
+            }
+            catch {
+                LocationTextBlock.Text = "Unable to get weather information.";
+            }
         }
     }
 }
